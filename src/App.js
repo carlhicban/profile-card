b807658d1dab9e1952755fd5f22be820ea798824
carlhicban <68343524+carlhicban@users.noreply.github.com>
2025-03-29 22:37:48 +0800

    Add props and components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,43 @@ import "./App.css";
 function App() {
   return (
     <div className="card">
-      {/* <Avatar/> */}
+      <Avatar />
       <div className="data">
-        {/* <Intro /> */}
+        <Intro />
 
-        {/* <Skillset /> */}
+        <SkillList />
       </div>
     </div>
   );
+}
+
+function Avatar() {
+  return <img className="avatar" src="logo512.png" alt="Carl" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>React Project</h1>
+      <p>
+        is simply dummy text of the printing and typesetting industry. Lorem
+        Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make{" "}
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div>
+      <Skill />
+    </div>
+  );
+}
+
+function Skill() {
+  return <div className="skill">React</div>;
 }
 
 export default App;
